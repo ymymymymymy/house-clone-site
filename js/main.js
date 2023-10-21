1cf3eaffeb8048f4cdd7d435ebc74e65c9f3f54e
ymymymymymy <mini11on7ym@gmail.com>
2023-10-22 00:36:15 +0900

    変更

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const ham = document.getElementById('js-hamburger');
 const nav = document.getElementById('js-nav');
 const bgc = document.getElementById('js-bgc');
+const pagetop = document.getElementById('js-pagetop');
 
 ham.addEventListener("click", function () {
     console.log('ok!');
@@ -9,4 +10,20 @@ ham.addEventListener("click", function () {
     bgc.classList.toggle('active');
 });
 
+// window.addEventListener("scroll", function () {
+//     const scroll = window.scrollY;
+//     if (scroll > 600) {
+//         pagetop.classList.add("active");
+//     } else {
+//         pagetop.classList.remove("active");
+//     }
+// });
 
+window.addEventListener("scroll", scroll_event);
+function scroll_event() {
+if (window.scrollY > 400) {
+    pagetop.style.opacity = "1";
+} else if (window.scrollY < 400) {
+    pagetop.style.opacity = "0";
+}
+}
