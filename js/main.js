06741edd4aa23ff6324556f241d415ea94e3e849
ymymymymymy <mini11on7ym@gmail.com>
2023-10-22 12:53:08 +0900

    変更

--- js/main.js
+++ js/main.js
@@ -3,21 +3,6 @@ const nav = document.getElementById('js-nav');
 const bgc = document.getElementById('js-bgc');
 const pagetop = document.getElementById('js-pagetop');
 
-ham.addEventListener("click", function () {
-    console.log('ok!');
-    ham.classList.toggle('active');
-    nav.classList.toggle('active');
-    bgc.classList.toggle('active');
-});
-
-// window.addEventListener("scroll", function () {
-//     const scroll = window.scrollY;
-//     if (scroll > 600) {
-//         pagetop.classList.add("active");
-//     } else {
-//         pagetop.classList.remove("active");
-//     }
-// });
 
 window.addEventListener("scroll", scroll_event);
 function scroll_event() {
@@ -27,3 +12,10 @@ if (window.scrollY > 400) {
     pagetop.style.opacity = "0";
 }
 }
+
+ham.addEventListener("click", function () {
+    console.log('ok!');
+    ham.classList.toggle('active');
+    nav.classList.toggle('active');
+    bgc.classList.toggle('active');
+});
